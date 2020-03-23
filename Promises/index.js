85511ddd7a365668cd64b062d97a6e57833788cc
m2lovric <m2lovric@gmail.com>
2020-03-23 16:58:18 +0100

    all

--- Promises/index.js
+++ Promises/index.js
@@ -1,1 +1,33 @@
+const inventory = {
+  sunglasses : 10,
+  pants : 1088,
+  bags : 1344 
+}
+
 console.log('Promises');
+
+const myExecutor = (res, rej) => {
+  if(inventory.sunglasses > 0){
+    res('Sunglasses order processed.');
+  }else{
+    rej('That item is sold out.');
+  }
+}
+
+const orderSunglasses = () => {
+  return new Promise(myExecutor);
+}
+const orderPromise = orderSunglasses();
+
+orderPromise
+.then((res) => {
+  let num = 0;
+  setInterval(() => {
+    console.log(`Order ${num}: ${res}`);
+    num++;
+  }, 2000);
+})
+.catch((err) => console.log(err));
+
+console.log("end");
+console.log("Promises Update");
